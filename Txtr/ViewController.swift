8a34b44ab7759537dd9a1508dc8cc03fbc7bb80f
Devon Yanitski <devon7y@gmail.com>
2019-01-05 13:48:20 -0700

    Initial Commit

---
+++ Txtr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Txtr
+//
+//  Created by Devon Yanitski on 2019-01-05.
+//  Copyright © 2019 Devon Yanitski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
